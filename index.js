8d9e938fc2528be3e5859f8aa520ff4488c7f4b8
Shaun <soo.shaun@gmail.com>
2019-08-23 17:36:06 +1000

    firts commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function shadowizard(options) {
+    // this is to create a standard class name that the user can use
+    let images = document.querySelectorAll('.shadowizard'); // user needs to place this standard class name to the element to style
+
+    if (options.shadow_type === 'hard') {
+        options.shadow_type = '0px';
+    } else {
+        options.shadow_type = '15px';
+    }
+
+    images.forEach(image => {
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0, 0, 0, 0.12 )`
+
+        if (options.padding === 'true') {
+            image.style.padding = `1rem`;
+        } else {
+            image.style.padding = `3rem`;
+        }
+    })
+}
+
+module.exports.shadowizard = shadowizard;
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Installation
+
+`npm - i - myfirstnpm --save`
+
+Then import to the file that you would like to use this feature
+
+import {shadowizard} from 'myfirstnpm'
+
+shadowizard({
+  shadow_type: 'soft' || 'hard',
+  padding: 'false' || 'true'
+});
